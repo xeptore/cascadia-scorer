f2497e92af09905bc0a94665061bb4d0df9f01ff
xeptore <29199390+xeptore@users.noreply.github.com>
2026-08-14 11:06:14 +0330

    feat: update PWA icons and manifest configuration
    - Added multiple new icon sizes (72x72, 96x96, 128x128, 144x144, 152x152, 192x192, 384x384, 512x512) for improved PWA support.
    - Removed old icon files (192.png, 512.png) and replaced them with new versions.
    - Updated the manifest to include the new icons with appropriate sizes and purposes.
    
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,17 @@ export default defineConfig({
     VitePWA({
       registerType: 'prompt',
       injectRegister: false,
-      includeAssets: ['icon-192.png', 'icon-512.png'],
+      includeAssets: [
+        'icon-72x72.png',
+        'icon-96x96.png',
+        'icon-128x128.png',
+        'icon-144x144.png',
+        'icon-152x152.png',
+        'icon-192x192.png',
+        'icon-384x384.png',
+        'icon-512x512.png',
+        'icon.png',
+      ],
       manifest: {
         name: 'Cascadia Scorekeeper',
         short_name: 'Cascadia Score',
@@ -20,17 +30,59 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: '/icon-192.png',
-            sizes: '192x192',
+            src: '/icon-72x72.png',
+            sizes: '72x72',
             type: 'image/png',
-            purpose: 'any maskable',
+            purpose: 'any maskable'
           },
           {
-            src: '/icon-512.png',
+            src: '/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable',
+            purpose: 'any maskable'
           },
+          {
+            src: '/icon.png',
+            sizes: 'any',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
         ],
       },
       workbox: {
